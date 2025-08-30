9b1c62d4ef95cca239a1bc8a158ab162f73aef5d
Napus Samuanpho <napus.dev@gmail.com>
2025-08-30 16:29:34 +0700

    Add user management module with CRUD operations and integrate dotenv and cookie-parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as CookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  dotenv.config();
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(CookieParser());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
