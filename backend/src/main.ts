b274e938fb7a35cc12ae0a53b27bcb3d03c86751
Napus Samuanpho <napus.dev@gmail.com>
2025-09-09 00:17:36 +0700

    refactor main.ts to use bootstrap function and clean up commented code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,42 +1,42 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import * as dotenv from 'dotenv';
-// import * as CookieParser from 'cookie-parser';
-// async function bootstrap() {
-//   dotenv.config();
-//   const app = await NestFactory.create(AppModule)
-//   app.setGlobalPrefix('api');
-//   app.useGlobalPipes(new ValidationPipe());
-//   app.use(CookieParser());
-//   app.enableCors();
-//   await app.listen(process.env.PORT ?? 3000);
-
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
-import * as functions from 'firebase-functions';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-dotenv.config();
-
-const server = express();
-
-async function createNestApp() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+import * as CookieParser from 'cookie-parser';
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
+  app.use(CookieParser());
   app.enableCors();
-  await app.init();
+  await app.listen(process.env.PORT ?? 3000);
+
 }
+bootstrap();
 
-createNestApp();
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as dotenv from 'dotenv';
+// import * as cookieParser from 'cookie-parser';
+// import * as functions from 'firebase-functions';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
 
-export const api = functions.https.onRequest(server);
+// dotenv.config();
+
+// const server = express();
+
+// async function createNestApp() {
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+//   app.setGlobalPrefix('api');
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.use(cookieParser());
+//   app.enableCors();
+//   await app.init();
+// }
+
+// createNestApp();
+
+// export const api = functions.https.onRequest(server);
