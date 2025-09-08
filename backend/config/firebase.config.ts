ee11d67665f3441d99915c9017edf9443bb6e5e4
Napus Samuanpho <napus.dev@gmail.com>
2025-09-08 16:47:35 +0700

    refactor: Update Firebase configuration to read service account key from environment variable

--- backend/config/firebase.config.ts
+++ backend/config/firebase.config.ts
@@ -1,10 +1,12 @@
 import * as admin from 'firebase-admin';
+import { readFileSync } from 'fs';
 
 if (!process.env.FIREBASE_KEY) {
   throw new Error('FIREBASE_KEY environment variable is not set.');
 }
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
+const serviceAccount = JSON.parse(
+  readFileSync('/etc/secrets/firebasekey.json', 'utf8')
+);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
