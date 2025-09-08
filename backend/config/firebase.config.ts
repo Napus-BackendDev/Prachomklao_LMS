434422cf0af3986aca6f57d6a154be21e11f6cc4
Napus Samuanpho <napus.dev@gmail.com>
2025-09-08 16:42:53 +0700

    refactor: Update Firebase configuration to use environment variable for service account key

--- backend/config/firebase.config.ts
+++ backend/config/firebase.config.ts
@@ -1,10 +1,9 @@
 import * as admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import * as path from 'path';
 
-const serviceAccount = JSON.parse(
-  readFileSync(path.resolve(process.cwd(), 'config/firebasekey.json'), 'utf8'),
-);
+if (!process.env.FIREBASE_KEY) {
+  throw new Error('FIREBASE_KEY environment variable is not set.');
+}
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
