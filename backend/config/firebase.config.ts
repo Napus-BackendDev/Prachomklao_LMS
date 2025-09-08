2005cfbcf10d4c842aa127cdb0d95983f613f7e7
Napus Samuanpho <napus.dev@gmail.com>
2025-09-09 00:00:43 +0700

    dd

--- backend/config/firebase.config.ts
+++ backend/config/firebase.config.ts
@@ -1,10 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as fs from 'fs';
 
-if (!process.env.FIREBASE_KEY) {
-  throw new Error('FIREBASE_KEY environment variable is not set.');
-}
-
 const serviceAccount = JSON.parse(
   fs.readFileSync('/etc/secrets/firebasekey.json', 'utf8'),
 );
