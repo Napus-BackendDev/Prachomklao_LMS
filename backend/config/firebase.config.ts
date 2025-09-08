e4171fc48d71d28f0b07b939e470e4e697f33bd6
Napus Samuanpho <napus.dev@gmail.com>
2025-09-08 21:43:50 +0700

    ada

--- backend/config/firebase.config.ts
+++ backend/config/firebase.config.ts
@@ -1,12 +1,12 @@
 import * as admin from 'firebase-admin';
-import { readFileSync } from 'fs';
+import * as fs from 'fs';
 
 if (!process.env.FIREBASE_KEY) {
   throw new Error('FIREBASE_KEY environment variable is not set.');
 }
-const serviceAccount = JSON.parse(
-  readFileSync('/etc/secrets/firebasekey.json', 'utf8')
-);
+
+const serviceAccount = JSON.parse(fs.readFileSync('/etc/secrets/firebasekey.json', 'utf-8'));
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
