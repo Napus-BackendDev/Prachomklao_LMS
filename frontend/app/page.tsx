1c4c76c1a36a9166f5474a132af72763cebf19f6
Napus Samuanpho <napus.dev@gmail.com>
2025-09-17 09:48:03 +0700

    fix: update layout and styling of CourseCard component; adjust image properties and container classes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -150,9 +150,12 @@ export default function Home() {
         className="flex flex-col items-center justify-center py-12 space-y-6 bg-gradient-to-b from-[#FFFFFF] to-[#F0F8FF]"
       >
         <p className="text-4xl font-semibold">หลักสูตรของเรา</p>
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 px-4 max-w-6xl">
+        <div className="flex items-center gap-4">
           {courses?.slice(0, 2).map((course: Courses) => (
-            <div key={course.id} className="w-full">
+            <div
+              key={course.id}
+              className="w-full h-64 shrink-0 basis-1/2 p-4"
+            >
               <CourseCard
                 title={course.title}
                 id={course.id ?? ""}
