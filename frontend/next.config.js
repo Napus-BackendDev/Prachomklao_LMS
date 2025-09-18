6854a98076f42ee00aca3efef1a11cbd7b38e70b
Napus Samuanpho <napus.dev@gmail.com>
2025-09-18 10:52:43 +0700

    fix nest.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const fs = require('fs');
-
 const dotenv = require('dotenv');
-
 const env = dotenv.parse(fs.readFileSync('.env'));
 
 const nextConfig = {
   reactStrictMode: true,
-  env: env
-}
+  env: env,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://prachomklao-lms.onrender.com/api/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
