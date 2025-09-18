95d3bcb696f62acc361c33cf5a67957044e4cd06
Napus Samuanpho <napus.dev@gmail.com>
2025-09-18 11:08:44 +0700

    fix: remove global prefix from API and update public API URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,6 @@ import * as CookieParser from 'cookie-parser';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(CookieParser());
   app.enableCors({
